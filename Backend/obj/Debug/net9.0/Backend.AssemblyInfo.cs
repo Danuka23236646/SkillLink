df7e73606cfd0a2fadc95080202b7474e173fb40
Danuka23236646 <danukbarnett@gmail.com>
2025-09-06 15:48:22 +0530

    up

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91388e48e9885ba2e371ed07d13dae4c43f14bfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cdfbdf21268fa38382dae8dd7a62ce89257f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
