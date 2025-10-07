8de08c3eda949a57c7c18348794e20cddea1842a
Danuka23236646 <danukbarnett@gmail.com>
2025-10-07 12:40:19 +0530

    npm

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ca035d4b7364d1293f9f530a407977eed2f843c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2051f7c553b97541aee83b2e7c9ba9a7c4785a64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
